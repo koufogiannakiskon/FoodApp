ee5e6583d549dfb1880572457ad784676b3d177b
Konstantinos K <koufogiannakiskon@gmail.com>
2022-11-04 16:09:18 +0200

    Add form with fetch

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -27,6 +27,8 @@ const cartReducer = (state, action) => {
             updatedItems = state.items.concat(action.item);
         }
 
+        if (action.type === 'CLEAR') return defaultCartState;
+
         return {
             items: updatedItems,
             totalAmount: updatedTotalAmount
@@ -65,11 +67,16 @@ const CartProvider = (props) => {
         dispatchCartAction({ type: 'REMOVE', id: id });
     };
 
+    const clearCartHandler = () => {
+        dispatchCartAction({ type: 'CLEAR' });
+    }
+
     const cartContext = {
         items: cartState.items,
         totalAmount: cartState.totalAmount,
         addItem: addItemToCartHandler,
-        removeItem: removeItemFromCartHandler
+        removeItem: removeItemFromCartHandler,
+        clearCart: clearCartHandler
     };
 
     return (<CartContext.Provider value={cartContext}>
