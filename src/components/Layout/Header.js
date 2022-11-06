b81d1239b48dec73f1dc124aa39a2190003a39de
Konstantinos K <koufogiannakiskon@gmail.com>
2022-11-06 16:43:30 +0200

    change prices

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,7 +8,7 @@ import classes from "./Header.module.css";
 const Header = props => {
     return (<Fragment>
         <header className={classes.header}>
-            <h1>React restaurant</h1>
+            <h1>Konstantinos Restaurant</h1>
             <HeaderCartButton onClick={props.onShowCart} />
         </header>
         <div className={classes['main-image']}>
